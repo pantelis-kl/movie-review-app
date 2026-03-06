4d794509261642b1849ebc9a9628df35c86a7d9d
pantelis-kl <panteliskleiousis@gmail.com>
2026-03-06 15:57:14 +0200

    adding event listeners

---
+++ index.js
@@ -0,0 +1,14 @@
+class MovieReview{
+    constructor(){
+        this.navbarItems=document.querySelectorAll(".navbar-item");
+        this.init();
+    }
+
+    init(){
+        this.initializeEventListeners();
+    }
+
+    initializeEventListeners(){
+        
+    }
+}
