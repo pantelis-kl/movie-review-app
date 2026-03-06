07b5159e39c97444c70c6cebb0779f6caf023e06
pantelis-kl <panteliskleiousis@gmail.com>
2026-03-06 18:49:57 +0200

    fetched data for movie image and title

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 class MovieReview{
     constructor(){
+        this.apiKey=config.apiKey;
         this.navbarItems=document.querySelectorAll(".navbar-item");
         this.searchField=document.getElementById("search-input");
         this.searchButton=document.getElementById("search-button");
         this.errorMessage=document.getElementById("error-message");
         this.loadingMessage=document.querySelector(".full-loading");
+        this.movieTitle=document.getElementById("movie-name");
+        this.moviesContainer=document.querySelector(".movies-container");
+        this.movieImage=document.getElementById("movie-image");
+        this.movieDescr=document.getElementById("movie-description");
         this.init();
     }
 
@@ -43,6 +48,112 @@ class MovieReview{
         document.querySelector(".movies-form").addEventListener("submit",event=>{
             event.preventDefault();
         })
+    }
+
+    async fetchData(){
+        try{
+            this.loadingMessage.style.display="flex";
+            const movieName=document.getElementById("search-input").value.toLowerCase();
+            if(movieName.trim()!==""){
+                const response= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${movieName}`);
+                if(!response.ok){
+                    this.getErrorMessage("Please enter a valid movie name");
+                    throw new Error("Cannot fetch the resource");
+                }
+                const movieData= await response.json();
+                try{
+                    const movieId=this.getMovieId(movieData);
+                    const movieDetailsData=await this.fetchMovieDetailsData(movieId);
+                    const movieCreditsData=await this.fetchCreditsData(movieId);
+                    const movieReviewsData=await this.fetchReviewsData(movieId);
+                    console.log(movieReviewsData);
+                    console.log(movieData);
+                    this.callFunctions(movieData,movieDetailsData,movieCreditsData,movieReviewsData);
+                }catch(error){
+                    console.error("Cannot find the movie ID");
+                    this.loadingMessage.style.display="none";
+                    this.getErrorMessage("Please enter a valid movie name");
+                }
+            }else{
+                this.getErrorMessage("Please enter a movie name");
+                this.loadingMessage.style.display="none";
+            }
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async fetchMovieDetailsData(movieId){
+        try{
+            const response=await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.apiKey}`);
+            if(!response.ok)
+                throw new Error("Cannot fetch the resource");
+            return await response.json();
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async fetchCreditsData(movieId){
+        try{
+            const response=await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${this.apiKey}`);
+            if(!response.ok)
+                throw new Error("Cannot fetch the resource");
+            return await response.json();
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async fetchReviewsData(movieId){
+        try{
+            const response=await fetch(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${this.apiKey}`);
+            if(!response.ok)
+                throw new Error("Cannot fetch the resource");
+            return await response.json();
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    callFunctions(movieData,movieDetailsData,movieCreditsData,movieReviewsData){
+        try{
+            if(movieData.results.length===0)
+                throw new Error("cannot fetch the resource");
+            this.loadingMessage.style.display="none";
+            this.errorMessage.style.display="none";
+            this.moviesContainer.style.display="flex";
+            this.getMovieTitle(movieData);
+            this.getMovieImage(movieData);
+            this.getMovieDescription(movieData);
+        }catch(error){
+            console.error(error);
+            this.getErrorMessage("Please enter a valid movie name");
+            this.loadingMessage.style.display="none";
+        }
+    }
+
+    getMovieTitle(movieData){
+        this.movieTitle.textContent=movieData.results[0].title;
+    }
+
+    getMovieImage(movieData){
+        const imageUrl=`https://image.tmdb.org/t/p/w500${movieData.results[0].poster_path}`;
+        this.movieImage.src=imageUrl;
+        this.movieImage.alt="Movie Image";
+    }
+
+    getMovieDescription(movieData){
+        this.movieDescr.textContent=movieData.results[0].overview;
+    }
+
+    getMovieId(movieData){
+        return movieData.results[0].id;
+    }
+
+    getErrorMessage(message){
+        this.errorMessage.style.display="block";
+        this.errorMessage.textContent=message;
     }
 
     customizeNavbarItems(index){
