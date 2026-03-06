4aa031a0a1a32f8a6b59f691cb6fd2adad9b5408
pantelis-kl <panteliskleiousis@gmail.com>
2026-03-06 18:32:00 +0200

    finished navbar

--- index.js
+++ index.js
@@ -9,6 +9,43 @@ class MovieReview{
     }
 
     initializeEventListeners(){
-        
+        let selectedIndex;
+        this.navbarItems.forEach((item,index)=>{
+            item.addEventListener("click",()=>{
+                selectedIndex=index;
+                this.customizeNavbarItems(index);
+            });
+
+            //hover navbar items
+            item.addEventListener("mouseover",()=>{
+                if(selectedIndex!==index)
+                    this.setActiveNavbarItem(item);
+            });
+            item.addEventListener("mouseout",()=>{
+                if(selectedIndex!==index)
+                    this.deactivateNavabarItem(item);
+            });
+        });
+    }
+
+    customizeNavbarItems(index){
+        this.navbarItems.forEach((item,currentIndex)=>{
+            if(index===currentIndex)
+                this.setActiveNavbarItem(item);
+            else
+                this.deactivateNavabarItem(item);
+        });
+    }
+
+    setActiveNavbarItem(item){
+        item.style.color="rgb(168, 168, 168)";
+        item.style.textDecoration="underline";
+    }
+
+    deactivateNavabarItem(item){
+        item.style.color="white";
+        item.style.textDecoration="none";
     }
 }
+
+new MovieReview();
